33e6a2d3cf2538913c54275d4bf1d0d42b6a585a
Ethan Drake <69392035+ethanjanadrake@users.noreply.github.com>
2021-01-12 15:50:00 -0800

    final

--- index.test.js
+++ index.test.js
@@ -1,38 +1,52 @@
+const assert = require('assert');
 const { forEach, map } = require('./index');
 
-let sum = 0;
-forEach(
-	[
-		1,
+// don't need this with mocha
+// const test = (desc, fn) => {
+// 	console.log('----', desc);
+// 	try {
+// 		fn();
+// 	} catch (err) {
+// 		console.log(err.message);
+// 	}
+// };
+
+it('The forEach function', () => {
+	let sum = 0;
+	forEach(
+		[
+			1,
+			2,
+			3
+		],
+		(value) => {
+			sum += value;
+		}
+	);
+
+	// Instead of making multiple if statements you can use assert.strictEqual (requirement)
+	// can add another argument which is a custom message
+	assert.strictEqual(sum, 6);
+});
+
+it('The map function', () => {
+	const result = map(
+		[
+			1,
+			2,
+			3
+		],
+		(value) => {
+			return value * 2;
+		}
+	);
+
+	// deepStrictEqual will judge the inner items in an object/array
+	assert.deepStrictEqual(result, [
 		2,
-		3
-	],
-	(value) => {
-		sum += value;
-	}
-);
+		4,
+		6
+	]);
+});
 
-if (sum !== 6) {
-	throw new Error('Expected summing array to equal 6');
-}
-
-const result = map(
-	[
-		1,
-		2,
-		3
-	],
-	(value) => {
-		return value * 2;
-	}
-);
-
-if (result[0] !== 2) {
-	throw new Error(`Expected to find 2, but found ${result[0]}`);
-}
-if (result[1] !== 4) {
-	throw new Error(`Expected to find 2, but found ${result[1]}`);
-}
-if (result[2] !== 6) {
-	throw new Error(`Expected to find 2, but found ${result[2]}`);
-}
+// npm install -g mocha
